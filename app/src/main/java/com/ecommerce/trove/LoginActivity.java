b9f5e7bbb8d743837417ba9ebd678e0d707bd869
Prince34998 <112163728+Prince34998@users.noreply.github.com>
2024-07-30 17:56:44 +0530

    feat(register): register design and form functionality

--- app/src/main/java/com/ecommerce/trove/LoginActivity.java
+++ app/src/main/java/com/ecommerce/trove/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.ecommerce.trove;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import android.os.Bundle;
 
@@ -11,7 +15,13 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.container), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, LoginFragment.newInstance())
